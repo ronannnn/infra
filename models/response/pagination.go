ffb498d178f23cb30237b9b5d9ed14df05702600
Ronan <crnoogle@gmail.com>
2024-04-04 19:38:45 +0800

    修改PageResult的内容

--- models/response/pagination.go
+++ models/response/pagination.go
@@ -1,14 +1,14 @@
 package response
 
-type TotalResult struct {
-	Field string  `json:"field"`
-	Total float64 `json:"total"`
+type StatsItem struct {
+	Field  string `json:"field"`
+	Result any    `json:"result"`
 }
 
 type PageResult struct {
-	List         any           `json:"list"`
-	Total        int64         `json:"total"` // total rows
-	PageNum      int           `json:"pageNum"`
-	PageSize     int           `json:"pageSize"`
-	TotalResults []TotalResult `json:"totalResults"` // totals of other fields, like container count, weight, volume, etc.
+	List     any         `json:"list"`
+	Total    int64       `json:"total"` // total rows
+	PageNum  int         `json:"pageNum"`
+	PageSize int         `json:"pageSize"`
+	Stats    []StatsItem `json:"stats"` // totals of other fields, like container count, weight, volume, etc.
 }
