bff857bb6d5ce26016f1ba802b9b103f05457687
Ronan <crnoogle@gmail.com>
2024-04-08 21:17:54 +0800

    fix typo

--- cfg/auth.go
+++ cfg/auth.go
@@ -5,5 +5,5 @@ type Auth struct {
 	RefreshTokenHourDuration int    `mapstructure:"refresh-token-hour-duration"` // Refresh Token有效时长
 	AccessTokenSecret        string `mapstructure:"access-token-secret"`         // Access Token加密字符串
 	RefreshTokenSecret       string `mapstructure:"refresh-token-secret"`        // Refresh Token加密字符串
-	Enable                   bool   `mapstructure:"enable"`                      // 是否启用jwt
+	Enabled                  bool   `mapstructure:"enabled"`                     // 是否启用jwt
 }
