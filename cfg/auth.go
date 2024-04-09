2688e0be4f640f256b1c785a2f04bd97f5c4a8eb
Ronan <crnoogle@gmail.com>
2024-04-09 21:53:38 +0800

    更新jwt刷新时间单位

--- cfg/auth.go
+++ cfg/auth.go
@@ -1,9 +1,9 @@
 package cfg
 
 type Auth struct {
-	AccessTokenHourDuration  int    `mapstructure:"access-token-hour-duration"`  // Access Token有效时长
-	RefreshTokenHourDuration int    `mapstructure:"refresh-token-hour-duration"` // Refresh Token有效时长
-	AccessTokenSecret        string `mapstructure:"access-token-secret"`         // Access Token加密字符串
-	RefreshTokenSecret       string `mapstructure:"refresh-token-secret"`        // Refresh Token加密字符串
-	Enabled                  bool   `mapstructure:"enabled"`                     // 是否启用jwt
+	AccessTokenMinuteDuration  int    `mapstructure:"access-token-minute-duration"`  // Access Token有效时长
+	RefreshTokenMinuteDuration int    `mapstructure:"refresh-token-minute-duration"` // Refresh Token有效时长
+	AccessTokenSecret          string `mapstructure:"access-token-secret"`           // Access Token加密字符串
+	RefreshTokenSecret         string `mapstructure:"refresh-token-secret"`          // Refresh Token加密字符串
+	Enabled                    bool   `mapstructure:"enabled"`                       // 是否启用jwt
 }
