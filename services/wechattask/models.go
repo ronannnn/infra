cb6f6afdeae1edeb7eb1a39aefa0b158dcca4657
Ronan <crnoogle@gmail.com>
2024-04-25 14:16:41 +0800

    修改uuid存储长度

--- services/wechattask/models.go
+++ services/wechattask/models.go
@@ -4,9 +4,9 @@ import "github.com/ronannnn/infra/models"
 
 type WechatTask struct {
 	models.Base
-	Uuid          *string            `json:"uuid" gorm:"type:varchar(255);uniqueIndex"` // 任务唯一标识
-	Name          *string            `json:"name"`                                      // 任务名称
-	Disabled      *bool              `json:"disabled"`                                  // 是否禁用该任务
+	Uuid          *string            `json:"uuid" gorm:"type:varchar(36);uniqueIndex"` // 任务唯一标识
+	Name          *string            `json:"name"`                                     // 任务名称
+	Disabled      *bool              `json:"disabled"`                                 // 是否禁用该任务
 	WechatUserIds *WechatTaskUserIds `json:"wechatUserIds" gorm:"many2many:wechat_tasks_user_ids"`
 }
 
