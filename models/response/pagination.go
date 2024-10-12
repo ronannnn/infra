59a1710ac7c6ec527020364eae5dcf84f17a53c5
Ronan <crnoogle@gmail.com>
2024-10-12 11:44:54 +0800

    优化stats数据结构

--- models/response/pagination.go
+++ models/response/pagination.go
@@ -5,10 +5,15 @@ type StatsItem struct {
 	Result any    `json:"result"`
 }
 
+type Stats struct {
+	StatsItems []StatsItem `json:"statsItems"`
+	Rmk        string      `json:"rmk"`
+}
+
 type PageResult struct {
-	List     any           `json:"list"`
-	Total    int64         `json:"total"` // total rows
-	PageNum  int           `json:"pageNum"`
-	PageSize int           `json:"pageSize"`
-	Stats    [][]StatsItem `json:"stats"` // totals of other fields, like container count, weight, volume, etc.
+	List      any     `json:"list"`
+	Total     int64   `json:"total"` // total rows
+	PageNum   int     `json:"pageNum"`
+	PageSize  int     `json:"pageSize"`
+	StatsList []Stats `json:"statsList"`
 }
